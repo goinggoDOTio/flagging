b099770a0fd9e24009a6fdb6aee468780e129f91
Joao Henrique Machado Silva <joaoh82@gmail.com>
2016-11-12 08:29:02 -0200

    Flagging done

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	nameFlag := flag.String("name", "[name]", "Put your name here")
+	booleanFlag := flag.Bool("really", false, "Add flag if it is true")
+	flag.Parse()
+
+	var reader = bufio.NewReader(os.Stdin)
+	fmt.Print("Hello, " + *nameFlag + ", please enter some text: ")
+	text, err := reader.ReadString('\n')
+	if err != nil {
+		log.Fatal("Error:", err)
+	}
+	fmt.Println(text)
+
+	fmt.Println("Really:", *booleanFlag)
 }
